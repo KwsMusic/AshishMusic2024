df5665b2a8d2c6a7bf7f8631b5860ed1dae6e423
UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>
2024-01-24 10:32:51 +0530

    Update play.py

--- AshishX/utils/inline/play.py
+++ AshishX/utils/inline/play.py
@@ -1,5 +1,6 @@
-import math
 
+import math
+from config import SUPPORT_CHAT, OWNER_ID
 from pyrogram.types import InlineKeyboardButton
 
 from AshishX.utils.formatters import time_to_seconds
@@ -15,7 +16,15 @@ def track_markup(_, videoid, user_id, channel, fplay):
             InlineKeyboardButton(
                 text=_["P_B_2"],
                 callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="💌 𝐎𝐖𝐍𝐄𝐑⏎", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
+            InlineKeyboardButton(
+                text="📝 𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url=SUPPORT_CHAT,
+            )
         ],
         [
             InlineKeyboardButton(
@@ -31,56 +40,48 @@ def stream_markup_timer(_, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
-    tgn = math.floor(percentage)
-    if 0 < tgn <= 10:
-        bar = "◉—————————"
-    elif 10 < tgn < 20:
-        bar = "—◉————————"
-    elif 20 <= tgn < 30:
-        bar = "——◉———————"
-    elif 30 <= tgn < 40:
-        bar = "———◉——————"
-    elif 40 <= tgn < 50:
-        bar = "————◉—————"
-    elif 50 <= tgn < 60:
-        bar = "—————◉————"
-    elif 60 <= tgn < 70:
-        bar = "——————◉———"
-    elif 70 <= tgn < 80:
-        bar = "———————◉——"
-    elif 80 <= tgn < 95:
-        bar = "————————◉—"
+    umm = math.floor(percentage)
+    if 0 < umm <= 10:
+        bar = "▁▄▂▇▄▅▄▅▃"
+    elif 10 < umm < 20:
+        bar = "▁▃▇▂🔮▅▇▄▅▃"
+    elif 20 <= umm < 30:
+        bar = "▃▁▇▂▅▃🍒▄▃▅"
+    elif 30 <= umm < 40:
+        bar = "▃▄▂▄🥃▇▅▃▅▁"
+    elif 40 <= umm < 50:
+        bar = "▁▃▄🧪▂▇▃▄▅▃"
+    elif 50 <= umm < 60:
+        bar = "▁▇▄💌▂▅▄▅▃▄"
+    elif 60 <= umm < 70:
+        bar = "▃▅▂🦠▅▇▁▄▃▁"
+    elif 70 <= umm < 80:
+        bar = "▁▃▅▇▂▅▄♦️▃▇"
+    elif 80 <= umm < 95:
+        bar = "▃▁▄▂▅💨▃▇▃▅"
     else:
-        bar = "—————————◉"
+        bar = "▃▅▂▅▇▁▄▃✄▁"
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="🦋", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="🥺", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="❤️", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="🍃", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="🌦", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
-        ],
+            ],
         [
             InlineKeyboardButton(
-                text="❤️‍🔥 ᴋɪɴɢ", url="https://t.me/BRANDEDKING82",
+                text="💌 𝐎𝐖𝐍𝐄𝐑⏎", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="sᴜᴘᴘᴏʀᴛ 💌", url="https://t.me/BRANDED_WORLD",
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                 text="🌹 sᴏᴜʀᴄᴇ 🌹", url=f"https://github.com/WCGKING/BRANDEDKING",
-            ),
-            InlineKeyboardButton(
-                text="🦋ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/BRANDED_PAID_CC",
-            ),
+                text="📝 𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url=SUPPORT_CHAT,
+            )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -90,27 +91,19 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
+            InlineKeyboardButton(text="🦋", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="🥺", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="❤️", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="🍃", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="🌦", callback_data=f"ADMIN Stop|{chat_id}")
+            ],
         [
             InlineKeyboardButton(
-                text="❤️‍🔥 ᴋɪɴɢ", url="https://t.me/BRANDEDKING82",
+                text="💌 𝐎𝐖𝐍𝐄𝐑⏎", url=f"tg://openmessage?user_id={OWNER_ID}",
             ),
             InlineKeyboardButton(
-                text="sᴜᴘᴘᴏʀᴛ 💌", url="https://t.me/BRANDED_WORLD",
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="🌹 sᴏᴜʀᴄᴇ 🌹", url=f"https://github.com/WCGKING/BRANDEDKING",
-            ),
-            InlineKeyboardButton(
-                text="🦋 ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/BRANDED_PAID_CC",
-            ),
+                text="📝 𝐒𝐮𝐩𝐩𝐨𝐫𝐭", url=SUPPORT_CHAT,
+            )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -122,11 +115,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"AyushPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"AyushPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
