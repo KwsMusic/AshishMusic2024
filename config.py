74a9fc2baf8b4de943fd1b108ea7721dc4efc90c
UNKNOWN-MUSIC <130531308+KwsMusic@users.noreply.github.com>
2024-01-24 10:11:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BWFTIME/AshishMusic",
+    "https://github.com/KwsMusic/AshishMusic2024",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Bwf")
 GIT_TOKEN = getenv(
